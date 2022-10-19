ea8782ecc5e5c55824872a436d5930af3a9b0830
Karpov Yuri <yurick@bk.ru>
2022-10-20 00:58:39 +0300

    feat: add ErrorPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import CurrencyConverter from '../CurrencyConverter/CurrencyConverter';
+import ExchangeRates from '../ExchangeRates/ExchangeRates';
+import ErrorPage from '../ErrorPage/ErrorPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route exact path='/' element={<CurrencyConverter />} />
+        <Route path='/rates' element={<ExchangeRates />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
